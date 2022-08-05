315730ffde327083675b08e8b761ea9541b2273c
sachingupta63 <sachingupta.6390@gmail.com>
2022-08-05 17:20:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/securetalk/Activity/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.securetalk.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.securetalk.Algo.AESActivity;
+import com.example.securetalk.Algo.DESActivity;
+import com.example.securetalk.Algo.MD5Activity;
+import com.example.securetalk.Algo.RSA_Activity;
+import com.example.securetalk.R;
+import com.example.securetalk.Steganography.DecodeSteganoActivity;
+import com.example.securetalk.Steganography.EncodeSteganoActivity;
+
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    AlertDialog.Builder builder;
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+    LinearLayout mMainLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        builder = new AlertDialog.Builder(this);
+        getSupportActionBar().hide();
+        mMainLayout= findViewById(R.id.main_layout);
+
+        BiometricManager biometricManager=BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this, "Device doesn't have fingerprint", Toast.LENGTH_SHORT).show();
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this, "Not Working", Toast.LENGTH_SHORT).show();
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this, "No fingerprint Assigned", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        Executor executor= ContextCompat.getMainExecutor(this);
+        biometricPrompt=new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                mMainLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+        promptInfo=new BiometricPrompt.PromptInfo.Builder().setTitle("Secure Talk")
+                .setDescription("Use Fingerprint to authenticate")
+                .setDeviceCredentialAllowed(true).build();
+        biometricPrompt.authenticate(promptInfo);
+
+
+
+    }
+    public void stego(View view)
+    {
+
+
+        builder.setMessage("Do you want to use Steganography ?")
+                .setCancelable(false)
+                .setPositiveButton("Encode", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Intent intent = new Intent( MainActivity.this, EncodeSteganoActivity.class  );
+                        startActivity(intent);
+                        Toast.makeText(getApplicationContext(),"Now You Can Encode The Image",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Decode", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent( MainActivity.this, DecodeSteganoActivity.class  );
+                        startActivity(intent);
+                        Toast.makeText(getApplicationContext(),"Now You Can Decode The Image",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }).setNeutralButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                    }
+                });
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("Steganography");
+        alert.show();
+    }
+
+
+
+    public void aes(View view)
+    {
+        Intent intent = new Intent( MainActivity.this, AESActivity.class  );
+        startActivity(intent);
+    }
+    public void des(View view)
+    {
+        Intent intent = new Intent( MainActivity.this, DESActivity.class  );
+        startActivity(intent);
+    }
+    public void rsa(View view)
+    {
+        Intent intent = new Intent( MainActivity.this, RSA_Activity.class  );
+        startActivity(intent);
+    }
+    public void md5(View view)
+    {
+        Intent intent = new Intent( MainActivity.this, MD5Activity.class  );
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/securetalk/Steganography/EncodeSteganoActivity.java
@@ -0,0 +1,254 @@
+package com.example.securetalk.Steganography;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ayush.imagesteganographylibrary.Text.AsyncTaskCallback.TextEncodingCallback;
+import com.ayush.imagesteganographylibrary.Text.ImageSteganography;
+import com.ayush.imagesteganographylibrary.Text.TextEncoding;
+import com.example.securetalk.Activity.MainActivity;
+import com.example.securetalk.BuildConfig;
+import com.example.securetalk.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+public class EncodeSteganoActivity extends AppCompatActivity implements TextEncodingCallback {
+
+    private static final int SELECT_PICTURE = 100;
+    private static final String TAG = "Encode Class";
+    //Created variables for UI
+    private TextView whether_encoded;
+    private ImageView imageView;
+    private EditText message;
+    private EditText secret_key;
+    //Objects needed for encoding
+    private TextEncoding textEncoding;
+    private ImageSteganography imageSteganography;
+    private ProgressDialog save;
+    private Uri filepath;
+    private File encodePath;
+    Boolean isSave=false,isEncoded=false;
+    //Bitmaps
+    private Bitmap original_image;
+    private Bitmap encoded_image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encode_stegano);
+
+        //initialized the UI components
+
+        whether_encoded = findViewById(R.id.whether_encoded);
+
+        imageView = findViewById(R.id.imageview);
+
+        message = findViewById(R.id.message);
+        secret_key = findViewById(R.id.secret_key);
+
+        Button choose_image_button = findViewById(R.id.choose_image_button);
+        Button encode_button = findViewById(R.id.encode_button);
+        Button save_image_button = findViewById(R.id.save_image_button);
+        Button img_send_button= findViewById(R.id.ImageSend);
+
+        checkAndRequestPermissions();
+
+
+        //Choose image button
+        choose_image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageChooser();
+            }
+        });
+        img_send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageSend();
+            }
+        });
+
+        //Encode Button
+        encode_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                whether_encoded.setText("");
+                if (filepath != null) {
+                    if (message.getText() != null) {
+
+                        //ImageSteganography Object instantiation
+                        imageSteganography = new ImageSteganography(message.getText().toString(),
+                                secret_key.getText().toString(),
+                                original_image);
+                        //TextEncoding object Instantiation
+                        textEncoding = new TextEncoding(EncodeSteganoActivity.this, EncodeSteganoActivity.this);
+                        //Executing the encoding
+                        textEncoding.execute(imageSteganography);
+                    }
+                }
+            }
+        });
+
+        //Save image button
+        save_image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isEncoded==false){
+                    Toast.makeText(EncodeSteganoActivity.this, "Encode First", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                final Bitmap imgToSave = encoded_image;
+                Thread PerformEncoding = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        saveToInternalStorage(imgToSave);
+                    }
+                });
+                save = new ProgressDialog(EncodeSteganoActivity.this);
+                save.setMessage("Saving, Please Wait...");
+                save.setTitle("Saving Image");
+                save.setIndeterminate(false);
+                save.setCancelable(false);
+                save.show();
+                PerformEncoding.start();
+            }
+        });
+
+    }
+
+    private void ImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    private void imageSend(){
+
+        if(isSave && isEncoded){
+            try {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(EncodeSteganoActivity.this, BuildConfig.APPLICATION_ID + ".provider",encodePath));
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                startActivity(Intent.createChooser(intent, "Share Image"));
+            }catch (Exception e){
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Save or Encode Image First", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Image set to imageView
+        if (requestCode == SELECT_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            filepath = data.getData();
+            try {
+                original_image = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+
+                imageView.setImageBitmap(original_image);
+            } catch (IOException e) {
+                Log.d(TAG, "Error : " + e);
+            }
+        }
+
+    }
+
+    // Override method of TextEncodingCallback
+
+    @Override
+    public void onStartTextEncoding() {
+        //Whatever you want to do at the start of text encoding
+    }
+
+    @Override
+    public void onCompleteTextEncoding(ImageSteganography result) {
+
+        //By the end of textEncoding
+
+        if (result != null && result.isEncoded()) {
+            isEncoded=true;
+            encoded_image = result.getEncoded_image();
+            whether_encoded.setText("Encoded");
+            imageView.setImageBitmap(encoded_image);
+        }
+    }
+
+    private void saveToInternalStorage(Bitmap bitmapImage) {
+        OutputStream fOut;
+        String timeStamp=new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), "Encoded" + timeStamp +".PNG"); // the File to save ,
+        encodePath=file;
+        try {
+            fOut = new FileOutputStream(file);
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fOut); // saving the Bitmap to a file
+            fOut.flush();
+            fOut.close(); // closing the stream
+            isSave=true;
+            whether_encoded.post(new Runnable() {
+                @Override
+                public void run() {
+                    save.dismiss();
+                }
+            });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkAndRequestPermissions() {
+        int permissionWriteStorage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int ReadPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (ReadPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (permissionWriteStorage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[0]), 1);
+        }
+    }
+
+}
